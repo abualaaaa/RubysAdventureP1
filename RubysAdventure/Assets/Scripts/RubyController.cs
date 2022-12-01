f318226780fe8145f7040b0ba3b9bab174de6957
abualaaaa <746951@student.dusd.net>
2022-12-01 09:57:19 -0800

    Finished collectibles /health
    finished collectibles health part, began damagables and almost done.

--- RubysAdventure/Assets/Scripts/RubyController.cs
+++ RubysAdventure/Assets/Scripts/RubyController.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed = 10.0f;
+
     public int maxHealth = 5;
+    public float timeInvincible = 2.0f;
+
+    public int health { get { return currentHealth; } }
     int currentHealth;
+
+    bool isInvincible;
+    float invincibleTimer;
 
     Rigidbody2D rigidbody2d;
     float horizontal;
@@ -15,7 +23,6 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-
         currentHealth = maxHealth;
     }
 
@@ -24,6 +31,13 @@ public class RubyController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
 
     }
 
@@ -36,8 +50,17 @@ public class RubyController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+    
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
